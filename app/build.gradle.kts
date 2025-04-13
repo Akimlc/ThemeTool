e954e72677de0d4eda8129d1c945b7a9f3959895
Akimlc <admin@akimlc.xyz>
2025-04-14 00:09:35 +0800

    ci:fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,39 +24,44 @@ android {
     }
     val properties = Properties()
     runCatching { properties.load(project.rootProject.file("local.properties").inputStream()) }
-
     val keystorePath = properties.getProperty("KEYSTORE_PATH") ?: System.getenv("KEYSTORE_PATH")
     val keystorePwd = properties.getProperty("KEYSTORE_PASS") ?: System.getenv("KEYSTORE_PASS")
     val alias = properties.getProperty("KEY_ALIAS") ?: System.getenv("KEY_ALIAS")
     val pwd = properties.getProperty("KEY_PASSWORD") ?: System.getenv("KEY_PASSWORD")
-
-    signingConfigs {
-        create("release") {
-            if (keystorePath!=null) {
+    if (keystorePath!=null) {
+        signingConfigs {
+            register("github") {
                 storeFile = file(keystorePath)
                 storePassword = keystorePwd
                 keyAlias = alias
                 keyPassword = pwd
                 enableV3Signing = true
+                enableV4Signing = true
+            }
+        }
+    } else {
+        signingConfigs {
+            register("release") {
+                enableV3Signing = true
+                enableV4Signing = true
             }
         }
     }
 
     buildTypes {
         release {
-            isShrinkResources = true
             isMinifyEnabled = true
+            isShrinkResources = true
+            vcsInfo.include = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig =
+                signingConfigs.getByName(if (keystorePath!=null) "github" else "release")
         }
         debug {
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            if (keystorePath!=null) signingConfig = signingConfigs.getByName("github")
         }
     }
 
