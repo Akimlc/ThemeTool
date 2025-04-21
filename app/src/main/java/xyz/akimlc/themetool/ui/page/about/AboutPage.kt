2e6da1ad12225a1f92d023eb8a3f4c5096efd4af
Akimlc <admin@akimlc.xyz>
2025-04-21 22:03:02 +0800

    style:update AboutPage.kt

--- app/src/main/java/xyz/akimlc/themetool/ui/page/about/AboutPage.kt
+++ app/src/main/java/xyz/akimlc/themetool/ui/page/about/AboutPage.kt
@@ -3,17 +3,21 @@ package xyz.akimlc.themetool.ui.page.about
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -24,112 +28,111 @@ import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.LazyColumn
 import top.yukonga.miuix.kmp.basic.MiuixScrollBehavior
 import top.yukonga.miuix.kmp.basic.Scaffold
+import top.yukonga.miuix.kmp.basic.ScrollBehavior
 import top.yukonga.miuix.kmp.basic.SmallTitle
 import top.yukonga.miuix.kmp.basic.Text
 import top.yukonga.miuix.kmp.basic.TopAppBar
 import top.yukonga.miuix.kmp.basic.rememberTopAppBarState
 import top.yukonga.miuix.kmp.extra.SuperArrow
+import top.yukonga.miuix.kmp.utils.overScrollVertical
 import xyz.akimlc.themetool.BuildConfig
 import xyz.akimlc.themetool.R
 
 @Composable
-fun AboutPage(navController: NavController) {
+fun AboutPage(
+    navController: NavController,
+    topAppBarScrollBehavior: ScrollBehavior,
+    padding: PaddingValues
+) {
     val uriHandler = LocalUriHandler.current
-    val scrollBehavior = MiuixScrollBehavior(rememberTopAppBarState())
     Scaffold(
         topBar = {
             TopAppBar(
                 title = "关于",
-                scrollBehavior = scrollBehavior,
+                scrollBehavior = topAppBarScrollBehavior
             )
         }
-    ) { padding ->
+    ) {
         LazyColumn(
-            modifier = Modifier.fillMaxHeight(),
-            topAppBarScrollBehavior = scrollBehavior,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 12.dp),
+            topAppBarScrollBehavior = topAppBarScrollBehavior,
             contentPadding = padding,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             item {
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                        .padding(top = 24.dp, bottom = 12.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_launcher_foreground), // 你的应用图标
-                        contentDescription = "App Icon",
-                        modifier = Modifier
-                            .size(200.dp)
-                            .clip(RoundedCornerShape(12.dp))
-                    )
                     Text(
                         text = "Theme Tool",
-                        fontSize = 20.sp,
+                        fontSize = 24.sp,
                         fontWeight = FontWeight.Bold
                     )
-                    Text(BuildConfig.VERSION_NAME, fontSize = 14.sp, color = Color.Gray)
+                    Spacer(modifier = Modifier.height(6.dp))
+                    Text(
+                        text = "版本号 ${BuildConfig.VERSION_NAME}",
+                        fontSize = 14.sp,
+                        color = Color.Gray
+                    )
                 }
             }
 
             item {
                 SmallTitle("参与人员")
-                Card(
-                    modifier = Modifier.padding(horizontal = 12.dp)
-                ) {
-                    SuperArrow(
-                        title = "Akimlc",
-                        summary = "Developer",
-                        onClick = {
-                            uriHandler.openUri("https://github.com/Akimlc")
-                        },
-                        leftAction = {
-                            Image(
-                                painter = painterResource(R.mipmap.ic_akimlc),
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .padding(end = 12.dp)
-                                    .size(38.dp)
-                                    .clip(RoundedCornerShape(48.dp))
-                            )
-                        }
-                    )
-                    //感谢列表
-                    SuperArrow(
-                        title = "感谢列表",
-                        onClick = {
-                            navController.navigate("ThanksPage")
-                        }
-                    )
+                Card(modifier = Modifier.fillMaxWidth()) {
+                    Column {
+                        SuperArrow(
+                            title = "Akimlc",
+                            summary = "Developer",
+                            onClick = {
+                                uriHandler.openUri("https://github.com/Akimlc")
+                            },
+                            leftAction = {
+                                Image(
+                                    painter = painterResource(R.mipmap.ic_akimlc),
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .padding(end = 12.dp)
+                                        .size(38.dp)
+                                        .clip(RoundedCornerShape(48.dp))
+                                )
+                            }
+                        )
+                        SuperArrow(
+                            title = "感谢列表",
+                            onClick = {
+                                navController.navigate("ThanksPage")
+                            }
+                        )
+                    }
                 }
-
             }
 
             item {
-                SmallTitle("交流/反馈")
-                Card(
-                    modifier = Modifier.padding(horizontal = 12.dp)
-                ) {
-                    SuperArrow(
-                        title = "频道",
-                        onClick = {
-                            uriHandler.openUri("https://t.me/Theme_Tool")
-                        }
-                    )
-                    SuperArrow(
-                        title = "群组",
-                        onClick = {
-                            uriHandler.openUri("https://t.me/ThemeToolChat")
-                        }
-                    )
+                SmallTitle("交流 / 反馈")
+                Card(modifier = Modifier.fillMaxWidth()) {
+                    Column {
+                        SuperArrow(
+                            title = "频道",
+                            onClick = { uriHandler.openUri("https://t.me/Theme_Tool") }
+                        )
+                        SuperArrow(
+                            title = "群组",
+                            onClick = { uriHandler.openUri("https://t.me/ThemeToolChat") }
+                        )
+                    }
                 }
             }
 
             item {
                 SmallTitle("引用")
                 Card(
-                    modifier = Modifier.padding(horizontal = 12.dp)
+                    modifier = Modifier.fillMaxWidth()
                 ) {
                     SuperArrow(
                         title = "MiuiX",
