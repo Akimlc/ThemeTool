3b4d365ced39b540c670681ad5fce2d68b7b0871
Akimlc <admin@akimlc.xyz>
2025-05-22 23:03:41 +0800

    chore: bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.2.3"
+        versionName = "1.3.0"
         versionCode = getGitCommitCount()
     }
     val properties = Properties()
