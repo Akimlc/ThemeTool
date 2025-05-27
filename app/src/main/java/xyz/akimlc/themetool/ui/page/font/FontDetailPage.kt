f49abbfe33a2123d84a68646e10677fc404d7818
Akimlc <admin@akimlc.xyz>
2025-05-27 12:46:42 +0800

    feat(font): 增加字体预览大图功能

--- app/src/main/java/xyz/akimlc/themetool/ui/page/font/FontDetailPage.kt
+++ app/src/main/java/xyz/akimlc/themetool/ui/page/font/FontDetailPage.kt
@@ -7,12 +7,14 @@ import android.content.Intent
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,17 +24,24 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.core.net.toUri
 import androidx.navigation.NavController
 import coil3.compose.AsyncImage
@@ -65,6 +74,9 @@ fun FontDetailPage(
     val fontDownloadUrl = fontData?.fontDownloadUrl
     val isLoading by viewModel.isLoading.collectAsState()
     val context = LocalContext.current
+
+    val selectedImageUrl = remember { mutableStateOf<String?>(null) }
+    val selectedImageIndex = remember { mutableStateOf<Int?>(null) }
 
     LaunchedEffect(uuid) {
         viewModel.loadFontData(uuid)
@@ -106,14 +118,16 @@ fun FontDetailPage(
                     horizontalArrangement = Arrangement.spacedBy(12.dp),
                     contentPadding = PaddingValues(horizontal = 8.dp)
                 ) {
-                    items(previewUrl) { imageUrl ->
+                    itemsIndexed(previewUrl) { index, imageUrl ->
                         AsyncImage(
                             model = imageUrl,
                             contentDescription = null,
                             modifier = Modifier
-                                .fillMaxSize()
                                 .width(224.dp)
                                 .height(498.dp)
+                                .clickable {
+                                    selectedImageIndex.value = index
+                                }
                         )
                     }
                 }
@@ -175,6 +189,29 @@ fun FontDetailPage(
                             colors = ButtonDefaults.textButtonColorsPrimary()
                         )
                     }
+                }
+            }
+        }
+        selectedImageUrl.value?.let { url ->
+            Dialog(
+                onDismissRequest = { selectedImageUrl.value = null },
+                properties = DialogProperties(
+                    usePlatformDefaultWidth = false,
+                    decorFitsSystemWindows = false
+                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black)
+                        .clickable { selectedImageUrl.value = null },
+                ) {
+                    AsyncImage(
+                        model = url,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
