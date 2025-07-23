bbe39371605c9aa342d75630e4a1e9e495fd401e
Akimlc <admin@akimlc.xyz>
2025-07-23 23:44:13 +0800

    refactor: 重构 HomePage，提升代码结构和性能

--- app/src/main/java/xyz/akimlc/themetool/ui/page/HomePage.kt
+++ app/src/main/java/xyz/akimlc/themetool/ui/page/HomePage.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
@@ -21,6 +22,14 @@ import xyz.akimlc.themetool.R
 import xyz.akimlc.themetool.ui.FontPageList
 import xyz.akimlc.themetool.ui.ThemePageList
 import xyz.akimlc.themetool.ui.compoent.SuperArrowItem
+
+
+data class MenuItem(
+    val titleRes: Int,
+    val iconRes: Int,
+    val route: String,
+    val onClick: (() -> Unit)? = null
+)
 
 @Composable
 fun HomePage(
@@ -43,63 +52,80 @@ fun HomePage(
             contentPadding = paddingValues
         ) {
             item {
-                SmallTitle(stringResource(id = R.string.title_theme))
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 12.dp)
-                        .padding(bottom = 6.dp)
-                        .padding(top = 6.dp)
-                ) {
-                    SuperArrowItem(
-                        title = stringResource(id = R.string.theme_search),
-                        icon = R.drawable.ic_search,
-                        onClick = {
-                            navController.navigate(ThemePageList.SEARCH)
-                        }
-                    )
-                    SuperArrowItem(
-                        title = stringResource(id = R.string.theme_parse),
-                        icon = R.drawable.ic_link,
-                        onClick = {
-                            navController.navigate(ThemePageList.PARSE)
-
-                        }
-                    )
-                }
+                ThemeSection(navController)
             }
             item {
-                SmallTitle(stringResource(id = R.string.title_font))
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 12.dp)
-                        .padding(bottom = 6.dp)
-                ) {
-                    SuperArrowItem(
-                        title = stringResource(id = R.string.font_search),
-                        icon = R.drawable.ic_font,
-                        onClick = {
-                            navController.navigate(FontPageList.SEARCH)
-                        }
-                    )
-                    SuperArrowItem(
-                        title = stringResource(id = R.string.font_convert),
-                        icon = R.drawable.ic_font_change,
-                        onClick = {
-                            navController.navigate(FontPageList.MTZ)
-                        }
-                    )
-                    SuperArrowItem(
-                        title = stringResource(id = R.string.font_module_convert),
-                        icon = R.drawable.ic_mask,
-                        onClick = {
-                            navController.navigate(FontPageList.ZIP)
-                        }
-                    )
-                }
+                FontSection(navController)
             }
         }
     }
+}
 
+@Composable
+fun ThemeSection(navController: NavController) {
+
+    val themeItems = remember {
+        listOf(
+            MenuItem(
+                titleRes = R.string.theme_search,
+                iconRes = R.drawable.ic_search,
+                route = ThemePageList.SEARCH
+            ),
+            MenuItem(
+                titleRes = R.string.theme_parse,
+                iconRes = R.drawable.ic_link,
+                route = ThemePageList.PARSE
+            )
+        )
+    }
+    Section(titleRes = R.string.title_theme, menuItems = themeItems, navController = navController)
+}
+
+@Composable
+fun FontSection(navController: NavController) {
+    val fontItem = remember {
+        listOf(
+            MenuItem(
+                titleRes = R.string.title_font_search,
+                iconRes = R.drawable.ic_search,
+                route = FontPageList.SEARCH
+            ),
+            MenuItem(
+                titleRes = R.string.font_convert,
+                iconRes = R.drawable.ic_font_change,
+                route = FontPageList.MTZ
+            ),
+            MenuItem(
+                titleRes = R.string.font_module_convert,
+                iconRes = R.drawable.ic_font_change,
+                route = FontPageList.MTZ
+            )
+        )
+    }
+    Section(titleRes = R.string.title_font, menuItems = fontItem, navController)
+}
+
+@Composable
+fun Section(
+    titleRes: Int,
+    menuItems: List<MenuItem>,
+    navController: NavController
+) {
+    SmallTitle(stringResource(id = titleRes))
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 12.dp)
+            .padding(bottom = 6.dp)
+    ) {
+        menuItems.forEach { item ->
+            SuperArrowItem(
+                title = stringResource(id = item.titleRes),
+                icon = item.iconRes,
+                onClick = {
+                    item.onClick?.invoke() ?: navController.navigate(item.route)
+                }
+            )
+        }
+    }
 }
