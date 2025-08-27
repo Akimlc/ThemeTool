585fe20639326e1c5cef0939eac4183bfe9943b7
Akimlc <admin@akimlc.xyz>
2025-08-27 10:57:20 +0800

    feat(font): 转换字体 添加字体预览功能

--- app/src/main/java/xyz/akimlc/themetool/ui/page/font/MtzFontPage.kt
+++ app/src/main/java/xyz/akimlc/themetool/ui/page/font/MtzFontPage.kt
@@ -1,7 +1,9 @@
 package xyz.akimlc.themetool.ui.page.font
 
 import android.app.Activity.RESULT_OK
+import android.content.Context
 import android.content.Intent
+import android.graphics.Typeface
 import android.net.Uri
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,12 +27,16 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
+import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.LinearProgressIndicator
 import top.yukonga.miuix.kmp.basic.MiuixScrollBehavior
 import top.yukonga.miuix.kmp.basic.Scaffold
+import top.yukonga.miuix.kmp.basic.SmallTitle
+import top.yukonga.miuix.kmp.basic.Text
 import top.yukonga.miuix.kmp.basic.TextButton
 import top.yukonga.miuix.kmp.extra.SuperDialog
 import top.yukonga.miuix.kmp.utils.overScrollVertical
@@ -173,6 +180,12 @@ fun MtzFontPage(navController: NavController) {
 
                 }
             }
+
+            //字体预览区域
+            item {
+                SmallTitle("字体预览区域")
+                FontPreview(fontUri)
+            }
         }
     }
 }
@@ -183,7 +196,6 @@ fun ProgressDialog(
     showDialog: MutableState<Boolean>,
     progress: Float,
 ) {
-
     SuperDialog(
         show = showDialog,
         title = stringResource(R.string.dialog_title_converting),
@@ -196,5 +208,55 @@ fun ProgressDialog(
 
 }
 
+@Composable
+fun FontPreview(fontUri: Uri?) {
+    val context = LocalContext.current
+    var fontFamily by remember { mutableStateOf<FontFamily?>(null) }
+    LaunchedEffect(fontUri) {
+        fontUri?.let {
+            val loadTypefaceFromUri = loadTypefaceFromUri(context, it)
+            fontFamily = loadTypefaceFromUri?.let { FontFamily(it) }
+        }
+    }
+    if (fontFamily!=null) {
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 12.dp)
+                .padding(vertical = 8.dp)
+        ) {
+            Text(
+                text = "简体 你 我 猫 狗 鱼 葱 兔 花 爱 心 的 一\n" +
+                        "每一杯普通的水，和喜欢的人在一起就会变成雪碧；他的胳膊和你轻轻碰到，你心里的气泡就会滋儿一下的跑到嗓子；甜甜的呀，还亮晶晶的，好像刚才喝下了整条星河。……\n" +
+                        "繁體 \n" +
+                        "每一杯普通的水，和喜歡的人在一起就會變成雪碧；他的胳膊和妳輕輕碰到，妳心裏的氣泡就會滋兒一下的跑到嗓子；甜甜的呀，還亮晶晶的，好像剛才喝下了整條星河。……\n" +
+                        "魃 魈 魁 魑 魅 魍 魉 爨 齁 （常规字库）\n" +
+                        "顛 萬 畝 長 遠 會 連 誇 稱 讚 過 麗 （1w）\n" +
+                        "屄 嚟 醃 歩 恆 姮 厷 痾 娿 幹 亜 燊 毐 掱（1.5w）\n" +
+                        "槑 烎 圐 圙 嫑 勥 奣 巭 謽 鬾 嘂 嚻 靐 龖 龘 齾 爩 龗 灪 龖 厵 鱻 犇 羴 驫 灥  嚞 孨 朤 讟 纞 竑 涐 烝 俢 婳（2w）\n" +
+                        "㙓 㵘 㐂 䲜 㥮 㑳 㤘 㑇 䋺 䀚（三万）\n" +
+                        "字母\n" +
+                        "abcdefghijklmnopqrstuvwxyz\n" +
+                        "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n" +
+                        "数字\n" +
+                        "1 2 3 4 5 6 7 8 9 0\n" +
+                        "符号 \n" +
+                        "逗号，顿号、句号。冒号：问号？ 感叹号！ \n" +
+                        "艾特@百分号% 井号# 括号( )省略号……",
+                fontFamily = fontFamily,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
 
 
+fun loadTypefaceFromUri(context: Context, uri: Uri): Typeface? {
+    return try {
+        context.contentResolver.openFileDescriptor(uri, "r")?.use { pfd ->
+            Typeface.Builder(pfd.fileDescriptor).build()
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
